1a60b308e3d0b34ebe312ca6cc0ea2b381cc044b
John Clever <jclever114@gmail.com>
2020-05-19 17:34:52 +0100

    fourth commit

--- index.js
+++ index.js
@@ -6,8 +6,15 @@
     document.querySelector('.toggleHome').addEventListener('click', function () {
         document.querySelector('.loader').style.display = 'block';
         document.querySelector('.select').style.display = 'none';
-    })
-
+    });
+    document.querySelector('.toggleBlogHome').addEventListener('click', function () {
+        document.querySelector('.loader').style.display = 'block';
+        document.querySelector('.blog').style.display = 'none';
+    });
+    document.querySelector('.toggleBlog').addEventListener('click', function () {
+        document.querySelector('.loader').style.display = 'block';
+        document.querySelector('.header').style.display = 'none';
+    });
 }
 
 
@@ -17,13 +24,22 @@
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('.header').style.display = 'none';
     document.querySelector('.select').style.display = 'block';
-    document.querySelector('.header_footer').style.display = 'none';
+    document.querySelector('.blog').style.display = 'none';
+    document.querySelector('.header_footer').style.display = 'block';
 
 }
 var homeFunc = function () {
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('.header').style.display = 'block';
     document.querySelector('.select').style.display = 'none';
+    document.querySelector('.blog').style.display = 'none';
+    document.querySelector('.header_footer').style.display = 'block';
+}
+var blogFunc = function () {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.header').style.display = 'none';
+    document.querySelector('.select').style.display = 'none';
+    document.querySelector('.blog').style.display = 'block';
     document.querySelector('.header_footer').style.display = 'block';
 }
 }
