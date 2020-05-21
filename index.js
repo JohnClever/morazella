f757a1f2222ef5deeee5b6c01003f7557cd51bf7
John Clever <jclever114@gmail.com>
2020-05-21 15:50:01 +0100

    fifth commit

--- index.js
+++ index.js
@@ -28,6 +28,10 @@
         document.querySelector('.news_p').style.margin_bottom = '10px';
         document.querySelector('.news_l').style.margin_bottom = '0px';
         document.querySelector('.news_h').style.margin_bottom = '0px';
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'block';
+        document.querySelector('.details_lifestyle').style.display = 'none';   
     });
     document.querySelector('.toggleBlogServ').addEventListener('click', function () {
         document.querySelector('.loader').style.display = 'block';
@@ -40,6 +44,10 @@
         document.querySelector('.news_p').style.margin_bottom = '10px';
         document.querySelector('.news_l').style.margin_bottom = '0px';
         document.querySelector('.news_h').style.margin_bottom = '0px';
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'block';
+        document.querySelector('.details_lifestyle').style.display = 'none';   
     });
     document.querySelector('.toggleBlog').addEventListener('click', function () {
         document.querySelector('.loader').style.display = 'block';
@@ -52,9 +60,15 @@
         document.querySelector('.news_p').style.margin_bottom = '10px';
         document.querySelector('.news_l').style.margin_bottom = '0px';
         document.querySelector('.news_h').style.margin_bottom = '0px';
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'block';
+        document.querySelector('.details_lifestyle').style.display = 'none';   
     });
     if (toggleBlogSport.addEventListener('click', function(){
         document.querySelector('.news_s').style.padding = '2px';
+        document.querySelector('.news_s').style.clip_path = `polygon(25% 0%, 100% 1%, 100% 100%, 0 100%, 0% 50%)
+        `;
         document.querySelector('.news_p').style.padding = '0px';
         document.querySelector('.news_l').style.padding = '0px';
         document.querySelector('.news_h').style.padding = '0px';
@@ -62,6 +76,11 @@
         document.querySelector('.news_p').style.margin_bottom = '0px';
         document.querySelector('.news_l').style.margin_bottom = '0px';
         document.querySelector('.news_h').style.margin_bottom = '0px';
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'none';
+        document.querySelector('.details_lifestyle').style.display = 'none';    
+        
         
 
     })) {
@@ -77,7 +96,11 @@
         document.querySelector('.news_p').style.margin_bottom = '10px';
         document.querySelector('.news_l').style.margin_bottom = '0px';
         document.querySelector('.news_h').style.margin_bottom = '0px';
-    })) {
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'none';
+        document.querySelector('.details_lifestyle').style.display = 'none'; 
+     })) {
         console.log('togglePolitic success');
         
     }
@@ -90,6 +113,10 @@
         document.querySelector('.news_p').style.margin_bottom = '0px';
         document.querySelector('.news_l').style.margin_bottom = '10px';
         document.querySelector('.news_h').style.margin_bottom = '0px';
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'none';
+        document.querySelector('.details_lifestyle').style.display = 'none'; 
     })) {
         console.log('togglePolitic success');
         
@@ -103,6 +130,10 @@
         document.querySelector('.news_p').style.margin_bottom = '0px';
         document.querySelector('.news_l').style.margin_bottom = '0px';
         document.querySelector('.news_h').style.margin_bottom = '10px';
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'none';
+        document.querySelector('.details_lifestyle').style.display = 'none'; 
     })) {
         console.log('togglePolitic success');
         
@@ -130,20 +161,44 @@
     document.querySelector('.header_footer').style.display = 'none';
 
 }
-var homeFunc = function () {
-    document.querySelector('.loader').style.display = 'none';
-    document.querySelector('.header').style.display = 'block';
-    document.querySelector('.select').style.display = 'none';
-    document.querySelector('.blog').style.display = 'none';
-    document.querySelector('.header_footer').style.display = 'block';
+    var homeFunc = function () {
+        document.querySelector('.loader').style.display = 'none';
+        document.querySelector('.header').style.display = 'block';
+        document.querySelector('.select').style.display = 'none';
+        document.querySelector('.blog').style.display = 'none';
+        document.querySelector('.header_footer').style.display = 'block';
 }
-var blogFunc = function () {
-    document.querySelector('.loader').style.display = 'none';
-    document.querySelector('.header').style.display = 'none';
-    document.querySelector('.select').style.display = 'none';
-    document.querySelector('.blog').style.display = 'block';
-    document.querySelector('.header_footer').style.display = 'block';
+    var blogFunc = function () {
+        document.querySelector('.loader').style.display = 'none';
+        document.querySelector('.header').style.display = 'none';
+        document.querySelector('.select').style.display = 'none';
+        document.querySelector('.blog').style.display = 'block';
+        document.querySelector('.header_footer').style.display = 'block';
 }
+    var toggleDetailsPolitics = function () {
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'block';
+        document.querySelector('.details_lifestyle').style.display = 'none';   
+}
+    var toggleDetailsSports = function () {
+        document.querySelector('.details_sports').style.display = 'block';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'none';
+        document.querySelector('.details_lifestyle').style.display = 'none';
+}
+    var toggleDetailsLifeStyle = function () {
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'none';
+        document.querySelector('.details_lifestyle').style.display = 'block';
+}
+    var toggleDetailsHealth = function () {
+        document.querySelector('.details_sports').style.display = 'none';
+        document.querySelector('.details_health').style.display = 'none';
+        document.querySelector('.details_politics').style.display = 'none';
+        document.querySelector('.details_lifestyle').style.display = 'block';      
+    }
 }
 
 
